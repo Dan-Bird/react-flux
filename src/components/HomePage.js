c21571433752aa03610fcb0023d0ee5c679d9a37
Daniel <daniel.bird@codurance.com>
2019-10-11 18:45:08 +0100

    Initial Commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Pluralsight Adminiatration</h1>
+      <p>React, Flux and React Router for ultra-respnsive web apps.</p>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default HomePage;
